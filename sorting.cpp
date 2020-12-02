cd2b5bbae6758d0beffa50daa216cc2bcd9dfe19
Jonathan Balina <43860681+JBalina@users.noreply.github.com>
2020-12-02 16:05:43 -0500

    Add files via upload

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,8 @@
 //Jonathan Balina
 
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -50,15 +52,63 @@ void quickSort(int arr[], int l, int h) {
 		quickSort(arr, pivot + 1, h);
 	}
 }
-/*
-void mergeSort() {
 
+void merge(int arr[], int l, int m, int r) {
+	int size1 = m - l + 1;
+	int size2 = r - m;
+	int tempArr1[size1], tempArr2[size2];
+
+	for (int i = 0; i < size1; i++) {
+		tempArr1[i] = arr[l + i];
+	}
+	for (int j = 0; j < size2; j++) {
+		tempArr2[j] = arr[m + 1 + j];
+	}
+
+	int i = 0;
+	int j = 0;
+	int k = l;
+
+	while(i < size1 && j < size2) {
+		if(tempArr1[i] <= tempArr2[j]) {
+			arr[k] = tempArr1[i];
+			i++;
+		}
+		else {
+			arr[k] = tempArr2[j];
+			j++;
+		}
+		k++;
+	}
+	while(i < size1) {
+		arr[k] = tempArr1[i];
+		i++;
+		k++;
+	}
+	while(j < size2) {
+		arr[k] = tempArr2[j];
+		j++;
+		k++;
+	}
 }
-*/
+
+void mergeSort(int arr[], int l, int r) {
+	if(l>=r)
+		return;
+	int m = (l + r) / 2;
+	mergeSort(arr, l, m);
+	mergeSort(arr, m+1, r);
+	merge(arr, l, m, r);
+}
+
+void mergeSort(int arr[], int n) {
+	mergeSort(arr, 0, n-1);
+}
+
 int main() {
 	int arr[] = {64, 34, 25, 12, 22, 11, 90};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	bubbleSort(arr, n);
+	mergeSort(arr, n);
 	printArray(arr, n);
 }
 
