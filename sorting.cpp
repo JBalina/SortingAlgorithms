c104b76cf5f66e2777b203d0e4905c3b87ab7b31
Jonathan Balina <43860681+JBalina@users.noreply.github.com>
2020-12-04 20:57:11 -0500

    Implemented heapsort and comparisons

--- sorting.cpp
+++ sorting.cpp
@@ -1,14 +1,17 @@
 //Jonathan Balina
 
 #include<iostream>
-#include<vector>
+#include<array>
 #include<algorithm>
+#include<chrono>
 
 using namespace std;
 
 void printArray(int arr[], int n) {
 	for(int i = 0; i < n; i++) {
 		cout << arr[i] << " ";
+		if((i+1) % 10 == 0)
+			cout << endl;
 	}
 	cout << endl;
 }
@@ -27,6 +30,50 @@ void bubbleSort(int arr[], int n) {
 			}
 		}
 	}
+}
+
+void insertionSort(int arr[], int n) {
+	int i, j, cur;
+	for(i = 1; i < n; i++) {
+		cur = arr[i];
+		j = i - 1;
+
+		while(j >= 0 && arr[j] > cur) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = cur;
+	}
+}
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+
+        heapify(arr, i, 0);
+    }
 }
 
 int partition(int arr[], int l, int h) {
@@ -51,6 +98,10 @@ void quickSort(int arr[], int l, int h) {
 		quickSort(arr, l, pivot - 1);
 		quickSort(arr, pivot + 1, h);
 	}
+}
+
+void quickSort(int arr[], int n) {
+	quickSort(arr, 0, n-1);
 }
 
 void merge(int arr[], int l, int m, int r) {
@@ -106,10 +157,47 @@ void mergeSort(int arr[], int n) {
 }
 
 int main() {
-	int arr[] = {64, 34, 25, 12, 22, 11, 90};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	mergeSort(arr, n);
+	srand(time(NULL));
+	const int n = 100000;
+	//const int n = 100;
+	int arr[n];
+	for(int i = 0; i < n; i++) {
+		arr[i] = rand() % 200000 + 1;
+		//arr[i] = rand() % 200 + 1;
+	}
+	int arrCopy[n];
+	copy(begin(arr), end(arr), begin(arrCopy));
+	cout << "Original array with " << n << " elements:" << endl;
 	printArray(arr, n);
+	chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+	bubbleSort(arr, n);
+	chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+	cout << "Sorted array:" << endl;
+	printArray(arr, n);
+	cout << "Bubble Sort: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << " milliseconds" << endl;
+	copy(begin(arrCopy), end(arrCopy), begin(arr));
+	t1 = chrono::steady_clock::now();
+	insertionSort(arr, n);
+	t2 = chrono::steady_clock::now();
+	cout << "Insertion Sort: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << " milliseconds" << endl;
+	copy(begin(arrCopy), end(arrCopy), begin(arr));
+	t1 = chrono::steady_clock::now();
+	heapSort(arr, n);
+	t2 = chrono::steady_clock::now();
+	cout << "Heap Sort: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << " milliseconds" << endl;
+	copy(begin(arrCopy), end(arrCopy), begin(arr));
+	t1 = chrono::steady_clock::now();
+	mergeSort(arr, n);
+	t2 = chrono::steady_clock::now();
+	cout << "Merge Sort: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << " milliseconds" << endl;
+	copy(begin(arrCopy), end(arrCopy), begin(arr));
+	t1 = chrono::steady_clock::now();
+	quickSort(arr, n);
+	t2 = chrono::steady_clock::now();
+	cout << "Quick Sort: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << " milliseconds" << endl;
+
+
+	//printArray(arrCopy, n);
 }
 
 
